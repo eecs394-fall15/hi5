58847b0d2eeb97fdf37ee741b44ccff5ab34639b
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-11-07 13:10:29 -0600

    Unopened hi5 mailbox display

---
+++ app/hi5/scripts/MailboxController.js
@@ -0,0 +1,28 @@
+angular
+  .module('hi5')
+  .controller('MailboxController', function($scope, supersonic, HighfiveParse, Highfive, Parse, UserParse) {
+    $scope.showSpinner = true;
+    $scope.highfives = null;
+
+    $scope.load = function() {
+      var query = new Parse.Query(HighfiveParse);
+      query.equalTo("opened", false);
+      query.equalTo("receiver", UserParse.current().id)
+      query.find().then(function(highfives) {
+          supersonic.logger.info("Successfully retrieved " + highfives.length + " highfives.");
+          $scope.$apply( function () {
+            $scope.highfives = highfives;
+            $scope.showSpinner = false;
+          });
+        },function(error) {
+          supersonic.logger.info("Error: " + error.code + " " + error.message);
+        });
+
+      // var unsubscribe = Task.all(queryParameters, options).whenChanged( function(updatedTasks) {
+      //   supersonic.logger.log("First element of updated Task collection: ", updatedTasks[0]);
+      // });
+      // unsubscribe();
+    };
+
+    $scope.load();
+  });
