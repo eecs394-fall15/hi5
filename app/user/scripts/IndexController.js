55211edb81db618b2c243de4a24808544cf595a1
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-11-07 13:11:22 -0600

    UserParse query logic

--- app/user/scripts/IndexController.js
+++ app/user/scripts/IndexController.js
@@ -10,16 +10,19 @@ angular
     //       $scope.showSpinner = false;
     //     });
     // });
-    $scope.load
-    var query = new Parse.Query(UserParse);
-    query.find().then(function(users) {
-        supersonic.logger.info("Successfully retrieved " + users.length + " users.");
-        $scope.$apply( function () {
-          $scope.users = users;
-          $scope.showSpinner = false;
+    $scope.load = function() {
+      var query = new Parse.Query(UserParse);
+      query.find().then(function(users) {
+          supersonic.logger.info("Successfully retrieved " + users.length + " users.");
+          $scope.$apply( function () {
+            $scope.users = users;
+            $scope.showSpinner = false;
+          });
+        },function(error) {
+          supersonic.logger.info("Error: " + error.code + " " + error.message);
         });
-      },function(error) {
-        supersonic.logger.info("Error: " + error.code + " " + error.message);
-      });
+    };
+
+    $scope.load();
 
   });
