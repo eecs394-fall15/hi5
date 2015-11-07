ad8d6303275540798b89e7cf410cf9307de05462
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-11-07 16:08:15 -0600

    test

--- app/hi5/scripts/SendController.js
+++ app/hi5/scripts/SendController.js
@@ -1,53 +1,102 @@
 angular
   .module('hi5')
-  .controller('SendController', function($scope, supersonic, User, UserParse, Parse) {
+  .controller('SendController', function($scope, supersonic, User, UserParse, Parse, Highfive) {
     $scope.numTimers = 0;
     $scope.watchID = null;
     $scope.timers = {
-    	giving : false 
+    	giving : false
     };
-    $scope.users = null;
+    $scope.users = [];
     $scope.buttonActive = 0;
+    var flag = false;
+
+
+    var selectedUsers = [];
 
     var query = new Parse.Query(UserParse);
     query.find().then(function(users) {
         supersonic.logger.info("Successfully retrieved " + users.length + " users.");
         $scope.$apply( function () {
-            $scope.users = users.map(function(user){user.selected=false; return user;});
+            $scope.users = users.map(function(user){
+              user.selected=false;
+              return user;
+            });
         });
     },function(error) {
         supersonic.logger.info("Error: " + error.code + " " + error.message);
     });
 
-    $scope.select = function (user) {
-        if (user.selected) {
-            $scope.buttonActive += 1;
-        } else {
-            $scope.buttonActive -= 1;
-        }
-        $scope.$apply($scope.buttonActive);
+    function getSelectedUsers(){
+      return $scope.users.filter(function(user){
+        return user.selected;
+      });
     }
+
+    function clearUsers(){
+      $scope.users = $scope.users.map(function(user){
+        user.selected = false;
+        return user;
+      })
+
+      $scope.enableButtons = false;
+      $scope.$apply($scope.enableButtons);
+    }
+
+    function enableButtons (){
+      return getSelectedUsers().length > 0;
+    }
+
+    $scope.select = function (user) {
+        $scope.enableButtons = enableButtons();
+        $scope.$apply($scope.enableButtons);
+        var index = selectedUsers.indexOf(user);
+
+        if(index < 0)
+          selectedUsers.push(user);
+        else {
+          selectedUsers.slice(index);
+        }
+    };
 
     $scope.initWatching = function(){
         $scope.watching = true;
     	supersonic.logger.info('Calling watch');
 		window.ondevicemotion = function(motionEvent){
 			var acc = motionEvent.acceleration;
-			supersonic.logger.info(acc.z);
+			// supersonic.logger.info(acc.z);
 		    setTimeout(function(){
                 if(Math.abs(acc.z)  > 10){
-                    $scope.$apply(function(){
-                        $scope.watching = false;
-                        $scope.highfive = "You are awesome";
-                    });
+                  flag = true;
                 }
 		    }, 2000);
 
-		    $scope.timers.giving = true;
+        if (flag){
+          $scope.$apply(function(){
+              var susers = getSelectedUsers();
+              for (var user in selectedUsers) {
+                highfive = {
+                  opened: false,
+                  receiver: user.id,
+                  sender: UserParse.current().id,
+                  senderName: UserParse.current().name,
+                  type: 'highfive'
+                };
+                supersonic.logger.log(user.id);
+                newhighfive = new Highfive(highfive);
+                newhighfive.save().then(function(){
+                    supersonic.logger.info("Hi5 sent!");
+                });
+              }
+              $scope.watching = false;
+              $scope.highfive = "You are awesome";
+          });
+        }
 
+		    $scope.timers.giving = true;
 		    $scope.numTimers++;
 	    };
-    }
+    };
+
 
     $scope.stopAccelerometer = function(timer){
     	supersonic.logger.info('Calling stop');
@@ -58,5 +107,5 @@ angular
     $scope.clear = function () {
         $scope.highfive = null;
         $scope.stopAccelerometer();
-    }
+    };
   });
