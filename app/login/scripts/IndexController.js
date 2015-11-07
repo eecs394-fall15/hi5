3d98852332dad10c4182327cd1826cea06d5409b
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-11-07 12:55:10 -0600

    login logic

--- app/login/scripts/IndexController.js
+++ app/login/scripts/IndexController.js
@@ -1,12 +1,24 @@
 angular
   .module('login')
-  .controller('IndexController', function($scope, supersonic) {
-  	$scope.login = function () {
-		var options = {
-		   duration: .5,
-		   curve: "easeInOut"
-		}
-			var animation = supersonic.ui.animate("slideFromLeft",options);
-		supersonic.ui.initialView.dismiss(animation);
-	}
+  .controller('IndexController', function($scope, UserParse, supersonic) {
+    $scope.error = null;
+
+
+    $scope.login = function() {
+      supersonic.logger.info("User: " + $scope.username);
+      supersonic.logger.info("Pass: " + $scope.password);
+      UserParse.logIn($scope.username, $scope.password).then(function(user) {
+        supersonic.logger.info("Login Succeed for {\"user\":" + $scope.username + ", \"password\":" + $scope.password + "\"}");
+        var options = {
+           duration: 0.5,
+           curve: "easeInOut"
+        }
+        var animation = supersonic.ui.animate("slideFromLeft",options);
+        supersonic.ui.initialView.dismiss(animation);
+      }, function(error) {
+        $scope.error = error;
+        supersonic.logger.info("Login Failed for {\"user\":" + $scope.username + ", \"password\":" + $scope.password + "\"}");
+      });
+    }
+
   });
