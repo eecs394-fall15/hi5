6e78bc724d943849dfad5d858dc4146d1cc6b69f
Aaron Leon <aaronleon2018@u.northwestern.edu>
2015-11-09 11:30:20 -0600

    View logic -- STILL NEED ANIMATION

--- app/hi5/scripts/MailboxController.js
+++ app/hi5/scripts/MailboxController.js
@@ -12,18 +12,27 @@ angular
           $scope.highfives = highfives;
         });
       });
-    }
+    };
 
-    // $scope.show = function(){
-    //   var modalView = new supersonic.ui.View("hi5#hand");
-    //   var options = {
-    //     animate: true
-    //   }
+    $scope.show = function(highfive){
+      var modalView = new supersonic.ui.View("hi5#view");
+      var options = {
+        animate: false
+      };
 
-    //   supersonic.ui.modal.show(modalView, options);
-    //   setTimeout(function(){
-    //     supersonic.ui.layers.pop();
-    //   }, 3000);
-    // }
+      supersonic.ui.modal.show(modalView, options).then(function(){
+        setTimeout(function(){
+          $scope.$apply(function() {
+            highfive.opened = true;
+            highfive.save().then(function(){});
+          });
+          supersonic.ui.modal.hide(options);
+        }, 2000);
+      });
+    };
+
+    supersonic.ui.views.current.whenVisible( function() {
+      $scope.loadHighfives();
+    });
 
   });
--- app/hi5/scripts/ViewController.js
+++ app/hi5/scripts/ViewController.js
@@ -1,6 +1,5 @@
 angular
   .module('hi5')
-  .controller('ViewController', function($scope, supersonic, $timeout) {
-    $timeout(supersonic.ui.layers.pop(), 2000);
-
+  .controller('ViewController', function($scope, supersonic) {
+    
   });
