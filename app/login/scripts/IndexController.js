d2a036d8a7952518692b43625a1c4d1da5e883cc
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-11-16 04:27:57 -0600

    Separated logic for logging in and handling login button

--- app/login/scripts/IndexController.js
+++ app/login/scripts/IndexController.js
@@ -3,12 +3,25 @@ angular
   .controller('IndexController', function($scope, UserParse, supersonic) {
     $scope.error = null;
 
-    $scope.login = function() {
+    $scope.handleLogin = function() {
       supersonic.logger.info("User: " + $scope.username);
       supersonic.logger.info("Pass: " + $scope.password);
+      parseLogin($scope.username, $scope.password, leaveLogin, errorHandler);
+    };
 
-      UserParse.logIn($scope.username, $scope.password).then(function(user) {
+    supersonic.ui.views.current.whenVisible(function() {
+      tryCachedLogin();
+    });
+
+    function parseLogin(username, password, successCb, failCb){
+      UserParse.logIn(username, password)
+      .then(function(err){
         supersonic.logger.info("Login Succeed for {\"user\":" + $scope.username + ", \"password\":" + $scope.password + "\"}");
+        successCb(user);
+      }, failCb);
+    }
+
+    function leaveLogin(user){
         var options = {
            duration: 0.5,
            curve: "easeInOut"
@@ -16,11 +29,17 @@ angular
 
         var animation = supersonic.ui.animate("slideFromLeft",options);
         supersonic.ui.initialView.dismiss(animation);
+    }
 
-      }, function(error) {
+    function errorHandler(error){
         $scope.error = error;
         supersonic.logger.log(error);
         supersonic.logger.info("Login Failed for {\"user\":" + $scope.username + ", \"password\":" + $scope.password + "\"}");
-      });
-    };
+    }
+
+
+    function tryCachedLogin(){
+      if(UserParse.current())
+        leaveLogin(null);
+    }
   });
