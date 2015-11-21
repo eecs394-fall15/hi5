f2f57e1678959443252c13b52edb48bf1f4ef87b
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-11-21 05:09:42 -0600

    Implemented statuses and viewing them

--- app/hi5/scripts/FriendRequestsController.js
+++ app/hi5/scripts/FriendRequestsController.js
@@ -3,6 +3,14 @@ angular
   .controller('FriendRequestController', function($scope, supersonic, Requests) {
     $scope.showSpinner = true;
     $scope.friendRequests = null;
+    $scope.statuses = [];
+
+    $scope.curUser = Requests.currentUser;
+
+    supersonic.ui.views.current.whenVisible(function() {
+      loadFriends();
+    });
+
 
     var contactListView = new supersonic.ui.View({
       location: "hi5#ContactList",
@@ -10,6 +18,18 @@ angular
     });
 
 
+    $scope.getStatus = function(user){
+      var status = user.get('status');
+      return status ? status : "No status currently";
+    }
+
+    $scope.handlePersonalStatus = function(){
+      displayStatusPrompt(setStatus);
+    };
+
+    $scope.replyFive = function(status){
+      supersonic.logger.log("replying to status");
+    };
 
     $scope.loadFriendRequests = function(){
       Requests.loadFriendRequests(function(error, friendRequests){
@@ -36,7 +56,35 @@ angular
       $scope.loadFriendRequests();
     });
 
+    function setStatus(promptdata){
+      var status = promptdata.input.trim();
+      
+      if(status.length > 40){
+        supersonic.ui.dialog.alert("Status too long")
+        displayStatusPrompt(setStatus);
+        return;
+      }
 
+      Requests.setStatus(status, function(err){
+        if(err)
+          supersonic.logger.log("Bad status")
+        else{
+          supersonic.logger.log("Successful status")
+          $scope.curUser = Requests.currentUser
+        }
+      });
+    } 
+
+    function displayStatusPrompt(successCb, defaultText){
+      defaultText = defaultText || "";
+
+      var options = {
+        title: "Set new status",
+        defaultText : defaultText
+      };
+
+      supersonic.ui.dialog.prompt("Set new status", options).then(successCb);
+    }
 
     // MENU BAR FUNCTIONS
 
@@ -57,5 +105,16 @@ angular
       }
     }).then(supersonic.ui.navigationBar.show());
 
+    function loadFriends(){
+      Requests.loadFriends(function(users) {
+        $scope.$apply( function () {
+          $scope.statuses = users.map(function(user){
+            user.selected=false;
+            return user;
+          });
+          $scope.showSpinner = false;
+        });
+      });
+    }
 
   });
